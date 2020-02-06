e53ed219934958a16c6d3cf23c1a3ddb09d2f3a6
dramiro <dramirobarragan@gmail.com>
2020-02-06 08:56:32 +0100

    end project administrador-pacientes

--- administrador-pacientes/src/App.js
+++ administrador-pacientes/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Formulario from './components/Formulario';
+import Cita from './components/Cita';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const citasIniciales = JSON.parse(localStorage.getItem('citas'))
+        ? JSON.parse(localStorage.getItem('citas'))
+        : [];
+
+    const [citas, setCitas] = useState(citasIniciales);
+
+    const crearCita = (cita) => {
+        setCitas([...citas, cita]);
+    };
+
+    const deleteCitaById = (id) => {
+        const nuevasCitas = citas.filter((cita) => cita.id !== id);
+        setCitas(nuevasCitas);
+    };
+
+    const addToLocalStorage = (listadoCitas) => {
+        localStorage.setItem('citas', JSON.stringify(listadoCitas));
+    };
+
+    useEffect(() => {
+        if (citasIniciales) {
+            addToLocalStorage(citas);
+        } else {
+            addToLocalStorage([]);
+        }
+    }, [citas]);
+
+    return (
+        <>
+            <h1>Administrador de pacientes</h1>
+            <div className='container'>
+                <div className='row'>
+                    <div className='one-half column'>
+                        <Formulario crearCita={crearCita} />
+                    </div>
+                    <div className='one-half column'>
+                        <h2>Administrar tus citas</h2>
+                        {citas.map((cita) => (
+                            <Cita
+                                key={cita.id}
+                                cita={cita}
+                                deleteCitaById={deleteCitaById}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
+
+
 
 export default App;
