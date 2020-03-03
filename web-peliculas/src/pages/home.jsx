bf4291dc0b6fe9a78a9e4d5161d2f904dbe05742
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-03-03 19:25:21 +0100

    añadido el movielist al home

--- web-peliculas/src/pages/home.jsx
+++ web-peliculas/src/pages/home.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import useFetch from '../hooks/useFetch';
 import { DB_MOVIES_DATA } from '../utils/constats';
 import SliderMovies from '../components/SliderMovies';
+import MovieList from '../components/MovieList/MovieList';
+import { Row, Col } from 'antd';
 
 const Home = () => {
     const url = `${DB_MOVIES_DATA.url}/movie/now_playing?api_key=${DB_MOVIES_DATA.api_key}&language=es-ES&page=1`;
+    const urlPopular = `${DB_MOVIES_DATA.url}/movie/popular?api_key=${DB_MOVIES_DATA.api_key}&language=es-ES&page=1`;
     const movies = useFetch(url);
+    const moviesPopular = useFetch(urlPopular);
     console.log('movies :', movies);
     return (
         <>
             <SliderMovies peliculas={movies} />
+            <Row>
+                <Col span={12}>
+                    <MovieList
+                        peliculas={moviesPopular}
+                        title={'Peliculas Populares'}
+                    />
+                </Col>
+                <Col span={12}>....</Col>
+            </Row>
         </>
     );
 };
