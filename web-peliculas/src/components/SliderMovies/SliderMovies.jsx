eb5d101f47dcd78ff636cf944e993b89e7ff4457
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-03-03 19:26:06 +0100

    añadido componente loading al slider

--- web-peliculas/src/components/SliderMovies/SliderMovies.jsx
+++ web-peliculas/src/components/SliderMovies/SliderMovies.jsx
@@ -4,18 +4,18 @@ import { Link } from 'react-router-dom';
 
 import './SliderMovies.scss';
 import Movie from '../Movie';
+import Loading from '../Loading/Loading';
 
 const SliderMovies = ({ peliculas }) => {
     const { data, error, loading } = peliculas;
 
     if (loading || !data) {
-        return 'Loading';
+        return <Loading />;
     }
 
     const { results } = data;
 
     return (
-        
         <Carousel autoplay className='slider-movies' effect='fade'>
             {results.map((movie) => (
                 <Movie key={movie.id} movie={movie} />
