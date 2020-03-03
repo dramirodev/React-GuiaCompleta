0cfbbce6f12897e90c5e2d97cf41b95dc294d569
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-03-03 19:24:17 +0100

    componente Loading

--- web-peliculas/src/components/Loading/Loading.jsx
+++ web-peliculas/src/components/Loading/Loading.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Spin } from 'antd';
 import './Loading.scss';
 const Loading = () => {
-    return <h2>loading</h2>;
+    return (
+        <div className='loading'>
+            <Spin size='large' />
+        </div>
+    );
 };
 
 export default Loading;
