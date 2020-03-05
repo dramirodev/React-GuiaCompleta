f4c1aa0adf2868859b7ed5920b324672847a52b3
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-03-05 21:06:10 +0100

    añdida descripcion de l apelicula

--- web-peliculas/src/pages/movie/movie.jsx
+++ web-peliculas/src/pages/movie/movie.jsx
@@ -24,7 +24,17 @@ const Movie = () => {
     );
 };
 
-const RenderMovie = ({ movie: { title, backdrop_path } }) => {
+const RenderMovie = ({
+    movie: {
+        title,
+        backdrop_path,
+        poster_path,
+        id,
+        release_date,
+        overview,
+        genres,
+    },
+}) => {
     const backdropPath = `${DB_MOVIES_DATA.url_imagen}${backdrop_path}`;
     return (
         <div
@@ -35,10 +45,58 @@ const RenderMovie = ({ movie: { title, backdrop_path } }) => {
         >
             <div className='movie__dark' />
             <Row>
-                <Col span={8} offset={3} className='movie__poster'></Col>
-                <Col span={10} className='movie__info'></Col>
+                <Col span={8} offset={3} className='movie__poster'>
+                    <PosterMovie imagen={poster_path} />
+                </Col>
+                <Col span={10} className='movie__info'>
+                    <MovieInfo
+                        title={title}
+                        id={id}
+                        release_date={release_date}
+                        overview={overview}
+                        genres={genres}
+                    />
+                </Col>
             </Row>
         </div>
+    );
+};
+
+const PosterMovie = ({ imagen }) => {
+    const posterPath = `${DB_MOVIES_DATA.url_imagen}${imagen}`;
+
+    return (
+        <div
+            style={{
+                backgroundImage: `url(${posterPath})`,
+            }}
+        />
+    );
+};
+
+const MovieInfo = ({ title, id, release_date, overview, genres }) => {
+    return (
+        <>
+            <div className='movie__info-header'>
+                <h1>
+                    {title}{' '}
+                    <span>
+                        {moment(release_date, 'YYYY-MM-DD').format('YYYY')}
+                    </span>
+                </h1>
+                <button>Ver Trailer</button>
+            </div>
+            <div className='movie__info-content'>
+                <h3>Información General</h3>
+                <p>{overview}</p>
+                <h3>Generos</h3>
+                <ul>
+                    {genres.map((g) => (
+                        <li key={g.id}>{g.name}</li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
 
