c0c74fabf53042ff64e122330c3e204fdc2fb0fe
David Ramiro Barragan <dramirobarragan@gmail.com>
2020-04-24 10:52:07 +0200

    primer commit proyecto producthunt con next y firebase

---
+++ produthuntnext/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Layout from '../components/layout/Layout';
+
+const Heading = styled.h1`
+    color: red;
+`;
+export default function Home() {
+    return (
+        <div>
+            <Layout>
+                <Heading>Inicio</Heading>
+            </Layout>
+        </div>
+    );
+}
---
+++ produthuntnext/pages/nosotros.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Layout from '../components/layout/Layout';
+
+const Heading = styled.h1`
+    color: red;
+`;
+export default function Nosotros() {
+    return (
+        <div>
+            <Layout>
+                <Heading>Nosotros</Heading>
+            </Layout>
+        </div>
+    );
+}
